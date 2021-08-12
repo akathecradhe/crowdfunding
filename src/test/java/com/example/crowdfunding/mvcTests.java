9198189538530cd9387fcee54585807f82664435
clive <tsunguc@cardiff.ac.uk>
2021-08-12 17:38:48 +0100

    made tests for searchByLocation and maxAmountTest

--- src/test/java/com/example/crowdfunding/mvcTests.java
+++ src/test/java/com/example/crowdfunding/mvcTests.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -68,6 +69,35 @@ public class mvcTests {
 
          //the size should only equal 1 to show thast it has matched 1 ID otherwise
         assertEquals(1,getByID.size());
+    }
+
+    @Test
+    public void searchByLocationTest() throws Exception{
+
+        fundraiserService.findByLocation("Braintree");
+
+        Predicate<Fundraiser> eqaulsLocation = x -> x.getLocation().equals("Braintree");
+
+        List<Fundraiser> location= allFundraisers
+                .stream().filter(eqaulsLocation).collect(Collectors.toList());
+
+        //the size should only equal 2 to show thast it has matched 2 locations
+        assertEquals(2,location.size());
+
+    }
+
+    @Test
+    public void maxAmountTest() throws Exception{
+
+         System.out.println(fundraiserService.findByMaxAmount(500).size());
+
+        Predicate<Fundraiser> eqaulsLessthan = x -> x.getTargetamount() <500;
+
+        List<Fundraiser> total= allFundraisers
+                .stream().filter(eqaulsLessthan).collect(Collectors.toList());
+        //the size should only equal 2 to show thast it has matched 2 locations
+        assertEquals(1,total.size());
+
     }
 
 
